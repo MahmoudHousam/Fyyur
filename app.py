e7e7d9a4310683a1da61d55c88ca36c2135c5ef7
MahmoudHousam <mahmoudhousam60@gmail.com>
2023-06-16 22:51:27 +0300

    replace with real data in def venues()

--- app.py
+++ app.py
@@ -113,27 +113,27 @@ def index():
 def venues():
   # TODO: replace with real venues data.
   #       num_upcoming_shows should be aggregated based on number of upcoming shows per venue.
-  data=[{
-    "city": "San Francisco",
-    "state": "CA",
-    "venues": [{
-      "id": 1,
-      "name": "The Musical Hop",
-      "num_upcoming_shows": 0,
-    }, {
-      "id": 3,
-      "name": "Park Square Live Music & Coffee",
-      "num_upcoming_shows": 1,
-    }]
-  }, {
-    "city": "New York",
-    "state": "NY",
-    "venues": [{
-      "id": 2,
-      "name": "The Dueling Pianos Bar",
-      "num_upcoming_shows": 0,
-    }]
-  }]
+  venue = Venue
+  show = Show
+  locations = venue.query.ordered_by(venue.city, venue.state).all()
+  data = []
+  for location in locations:
+    location_venues = venue.query.filter_by(state=location.city).filter_by(state=locations.state).all()
+    data.append(
+      {
+      "city": location.city,
+      "state": location.state,
+      "venues": [{
+        "id": venue.id,
+        "name": venue.name,
+        "num_upcoming_shows": len(
+          db.session.query(show).filter(show.venue_id == venue.id).filter(show.start_time > datetime.now()).all()
+        )
+      }
+      for venue in location_venues
+      ]
+      }
+    )
   return render_template('pages/venues.html', areas=data);
 
 @app.route('/venues/search', methods=['POST'])
